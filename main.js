cdb3b73535b120c01381571adae125b1ca19ee75
raymond-git <52261775+raymond-git@users.noreply.github.com>
2019-07-17 18:02:11 -0700

    7/17/019
    Edited some codes

--- main.js
+++ main.js
@@ -9,8 +9,7 @@ function getParameterByName(name, url) {
   }
   
   var getAllRecords = function() {
-    $.getJSON('https://api.airtable.com/v0/app0DTErYuJT7Yv4J/Table%201 \
-    -H "Authorization: Bearer keyFARPhuKuM0vln2',
+    $.getJSON('https://api.airtable.com/v0/app0DTErYuJT7Yv4J/Table%201?api_key=keyFARPhuKuM0vln2',
       function(airtable){
         var html = [];
         $.each(airtable.records, function(index, record) {
@@ -19,27 +18,48 @@ function getParameterByName(name, url) {
           var price = record.fields['Price'];
           var attachment = record.fields['Attachments'];
           var hotellocation = record.fields['Hotel Locations'];
-          var descriptions = record.fileds['Descriptions'];
-          var touristAttractions = record.fields['Tourist Attractions'];
-          var touristAttachments = record.fields['Tourist Attractions Attachment'];
+          var description = record.fields['Descriptions'];
+          var touristAttraction = record.fields['Tourist Attractions'];
+          var touristAttachment = record.fields['Tourist Attractions Attachments'];
           var triplocation = record.fields['Trip Locations'];
-          var tripDescriptions = record.fields['Trip Descriptions'];
-          html.push(listView(id, hotel, price, attachment, hotellocation, descriptions, touristAttractions,
-                             touristAttachments, triplocation, tripDescriptions));
+          var tripDescription = record.fields['Trip Descriptions'];
+          html.push(listView(id, hotel, price, attachment, hotellocation, description, touristAttraction,
+                             touristAttachment, triplocation, tripDescription));
         });
         $('.list-view').append(html);
       }
     );
   }
 
-  var listView = function(id, picture, jobTitle, avgRate) {
+  var listView = function(id) {
     return `
-      <div class="card border-dark" style="width: 18rem;">
-      ${picture ? `<img src="${picture[0].url}">` : ``}
-      <div class="card-body">
-        <h2 class="card-title"><a href="index.html?id=${id}">${jobTitle}</h2></a>
-        <p class="card-text"><u>Average Annual Salary:</u><p> $${toCommas(avgRate*24)}</p>
-        </div>
-      </div>
+      <h1>${id}</h1>
     `;
+  }
+
+  
+  var getOneRecord = function(id) {
+    $.getJSON(`https://api.airtable.com/v0/app0DTErYuJT7Yv4J/Table%201/${id}?api_key=keyFARPhuKuM0vln2`,
+      function(record){
+        var html = [];
+        var price = record.fields['Price'];
+        
+        html.push(detailView(price ));
+        $('.detail-view').append(html);
+      }
+    );
+  }
+
+
+  var detailView = function(price) {
+    return `
+  <h1>${price}</h1>
+    `;
+  }
+  
+  var id = getParameterByName('id');
+  if (id) {
+    getOneRecord(id);
+  } else {
+    getAllRecords();
   }
