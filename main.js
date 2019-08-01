815da61d9606cc6040b2f75efbeb7b60aef148dd
raymond-git <52261775+raymond-git@users.noreply.github.com>
2019-08-01 15:00:42 -0700

    8/1/2019

--- main.js
+++ main.js
@@ -7,56 +7,87 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
   }
-  
-  var getAllRecords = function() {
-    $.getJSON('https://api.airtable.com/v0/app0DTErYuJT7Yv4J/Table%201?api_key=keyFARPhuKuM0vln2',
+
+var getAllRecords = function(){
+  $.getJSON('https://api.airtable.com/v0/app0DTErYuJT7Yv4J/Table%201?api_key=keyFARPhuKuM0vln2',
       function(airtable){
         var html = [];
         $.each(airtable.records, function(index, record) {
           var id = record.id;
+          var attachment = record.fields['Attachments'];
           var hotel = record.fields['Hotels'];
           var price = record.fields['Price'];
-          var attachment = record.fields['Attachments'];
-          var hotellocation = record.fields['Hotel Locations'];
           var description = record.fields['Descriptions'];
-          var touristAttraction = record.fields['Tourist Attractions'];
-          var touristAttachment = record.fields['Tourist Attractions Attachments'];
-          var triplocation = record.fields['Trip Locations'];
-          var tripDescription = record.fields['Trip Descriptions'];
-          html.push(listView(id, hotel, price, attachment, hotellocation, description, touristAttraction,
-                             touristAttachment, triplocation, tripDescription));
+         
+          html.push(listView(id, attachment, hotel, price, description));
         });
         $('.list-view').append(html);
       }
     );
   }
 
-  var listView = function(id) {
+
+
+
+  var listView = function(id, attachment, hotel, price ) {
     return `
-      <h1>${id}</h1>
+    
+    <div class="card">
+  <div class="card">
+  ${attachment ? `<img src="${attachment[0].url}">` : ``}
+    <div class="card-body">
+      <h5 class="card-title">${hotel}</h5>
+      <p class="card-text">Price: $${price}</p>
+      <p> <a href="index2.html?id=${id}">Click here for more information</a></p>
+    </div>
+  </div>
     `;
   }
 
-  
+
   var getOneRecord = function(id) {
     $.getJSON(`https://api.airtable.com/v0/app0DTErYuJT7Yv4J/Table%201/${id}?api_key=keyFARPhuKuM0vln2`,
       function(record){
         var html = [];
-        var price = record.fields['Price'];
+        var attachment = record.fields['Attachments'];
+        var hotel = record.fields['Hotels'];
+        var description = record.fields['Descriptions'];
+        var hotellocation = record.fields['Hotel Locations'];
         
-        html.push(detailView(price ));
+        
+        html.push(detailView(attachment, hotel, description, hotellocation ));
         $('.detail-view').append(html);
       }
     );
   }
 
 
-  var detailView = function(price) {
+  var detailView = function(attachment, description, hotellocation) {
     return `
-  <h1>${price}</h1>
-    `;
-  }
-  
+    <div class="info1">
+    <div class="card-deck">
+      <div class="card border-dark" style="width: 18rem;">
+        ${attachment ? `<img src="${attachment[0].url}">` : ``}
+          <div class="card-body">
+            <h2 class="card-title">${description}</h2> 
+         </div> 
+      </div>
+      
+       <div class="card border-dark" style="width: 18rem;">
+        <div class="card-body">
+          <h2 class="card-title">Description</h2> 
+          <p class="card-text">${hotellocation}</p>
+        </div>
+       </div>    
+      
+     </div>   
+    </div>  
+      <div class="back">
+      <p><button type="button"><a href="index2.html">Back</button></a>
+      </div> 
+      `;
+    }
+    
   var id = getParameterByName('id');
   if (id) {
     getOneRecord(id);
---
+++ main2.js
@@ -0,0 +1,105 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  }
+
+var getAllRecords = function(){
+  $.getJSON('https://api.airtable.com/v0/app0DTErYuJT7Yv4J/Table%201?api_key=keyFARPhuKuM0vln2',
+      function(airtable){
+        var html = [];
+        $.each(airtable.records, function(index, record) {
+            var id = record.id;
+            var touristAttractions = record.fields['Tourist Attractions'];
+            var touristPhotos = record.fields['Tourist Photos'];
+           
+            
+          
+         
+          html.push(listView(id, touristAttractions, touristPhotos));
+        });
+        $('.list-view').append(html);
+      }
+    );
+  }
+
+
+
+
+  var listView = function(id, touristPhotos, touristAttractions ) {
+    return `
+    
+    <div class="card">
+    <div class="card">
+    ${touristPhotos ? `<img src="${touristPhotos[0].url}">` : ``}
+      <div class="card-body">
+        <h5 class="card-title">${touristAttractions}</h5>
+        <p> <a href="index3.html?id=${id}">Click here for more information</a></p>
+      </div>
+    </div>
+      `;
+    }
+
+
+  var getOneRecord = function(id) {
+    $.getJSON(`https://api.airtable.com/v0/app0DTErYuJT7Yv4J/Table%202/${id}?api_key=keyFARPhuKuM0vln2`,
+      function(record){
+        var html = [];
+        var touristAttractions = record.fields['Tourist Attractions'];
+        var touristPhotos = record.fields['Tourist Photos'];
+        var tripLocations = record.fields['Trip Locations'];
+        var tripDescriptions = record.fields['Trip Descriptions'];
+        
+        
+        
+        html.push(detailView( touristAttractions, touristPhotos, tripLocations, tripDescriptions));
+        $('.detail-view').append(html);
+      }
+    );
+  }
+
+
+  var detailView = function(touristPhotos, touristAttractions, tripDescriptions, tripLocations) {
+    return `
+    <div class="info">
+    <div class="card-deck">
+      <div class="card border-dark" style="width: 18rem;">
+        ${touristPhotos ? `<img src="${touristPhotos[0].url}">` : ``}
+        <div class="card-body">
+            <h2 class="card-title">${touristAttractions}</h2> 
+           
+          </div> 
+      </div>
+      
+       <div class="card border-dark" style="width: 18rem;">
+        <div class="card-body">
+          <h2 class="card-title">Job Description</h2> 
+          <p class="card-text">${tripDescriptions}</p>
+        </div>
+       </div>    
+
+       <div class="card border-dark" style="width: 18rem;">
+        <div class="card-body">
+          <h2 class="card-title">Job Description</h2> 
+          <p class="card-text">${tripLocations}</p>
+        </div>
+       </div>   
+      
+     </div>   
+    </div>  
+      <div class="back">
+      <p><button type="button"><a href="index.html">Back</button></a>
+      </div> 
+      `
+    }
+    
+  var id = getParameterByName('id');
+  if (id) {
+    getOneRecord(id);
+  } else {
+    getAllRecords();
+  }
